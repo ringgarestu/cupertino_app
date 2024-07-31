dad4e27e724d110a89fa2504df3af46b0910b83d
ringgavertika <ringgarestu@gmail.com>
2024-07-31 18:49:07 +0700

    cupertino_app

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.systemOrange,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Cupertino App'),
+      ),
+      child: Center(
+        child: Text(
+          'Home Page',
+          style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+        ),
+      ),
+    );
+  }
+}
